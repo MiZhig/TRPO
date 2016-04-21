1361e15fe3ef82e7d4efa2fa1dc40696a939ffc9
MiZhig <mihazhigalov@gmail.com>
2016-04-21 20:24:08 +0600

    Realizaciya

--- Labs/2/main.c
+++ Labs/2/main.c
@@ -23,3 +23,25 @@ main()
             printf ("x = %g", -c/b);
         }
     }
+    else
+    {
+        float d, x1, x2;
+        d = b * b - 4 * a * c;
+        if(d < 0)
+        {
+            printf("Korney net");
+        }
+        else
+        {
+            if(d == 0)
+            {
+                printf("x= %g", -b/(2*a));
+            }
+            else
+            {
+                printf("x1= %g, x2= %g",(-b+sqrt(d))/(2*a),(-b-sqrt(d))/(2*a));
+            }
+        }
+    }
+    return 0;
+}
