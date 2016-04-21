a7e9e4758a938a903838056d60c00c00790ae04f
MiZhig <mihazhigalov@gmail.com>
2016-04-21 20:18:51 +0600

    Proverka dannyh + nachalo realizacii

--- Labs/2/main.c
+++ Labs/2/main.c
@@ -5,3 +5,21 @@ main()
     float a,b,c;
     printf("Vvedite a, b, c cherez probel: ");
     scanf("%f %f %f", &a, &b, &c);
+    if(a == 0)
+    {
+        if(b == 0)
+        {
+            if(c == 0)
+            {
+                printf("x - Luboe chislo");
+            }
+            else
+            {
+                printf("Korney net");
+            }
+        }
+        else
+        {
+            printf ("x = %g", -c/b);
+        }
+    }
